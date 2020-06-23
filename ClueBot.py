9c66ba2e2f7c38b7aed983d9327938068bc937c4
Peter Gustafson <petergustafson19@gmail.com>
2020-06-23 14:30:55 -0700

    condensed the code and added remove button

--- ClueBot.py
+++ ClueBot.py
@@ -1,51 +1,52 @@
 import tkinter as tk
 from cards import card_handler
 
+suspects, weapons, rooms = card_handler.loadCards()
+
 root = tk.Tk()
 
+def removeFunction():
+    pass
+#Frame that holds the suspects, weapons and rooms ----------------
 swrFrameBG = "black"
 swrFrame = tk.Frame(root, bg=swrFrameBG, borderwidth = 5, relief = "solid")
 swrFrame.pack()
 
+#button to remove the selected options
+tk.Button(swrFrame, text="remove", command=removeFunction).pack(side=tk.BOTTOM, fill="x")
 
 #suspect section
 suspectVar = tk.StringVar()
 suspectBG = "#8bf7ec"
 suspectFrame = tk.Frame(swrFrame, bg=swrFrameBG, borderwidth = 1, relief = "groove")
 suspectFrame.pack(side = tk.LEFT, fill="y")
-tk.Radiobutton(suspectFrame, text = "mrs. white", variable = suspectVar, value = "mrs. white", bg=suspectBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(suspectFrame, text = "mrs. peacock", variable = suspectVar, value = "mrs. peacock", bg=suspectBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(suspectFrame, text = "miss scarlet", variable = suspectVar, value = "miss scarlet", bg=suspectBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(suspectFrame, text = "colonel mustard", variable = suspectVar, value = "colonel mustard", bg=suspectBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(suspectFrame, text = "mr. green", variable = suspectVar, value = "mr. green", bg=suspectBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(suspectFrame, text = "professor plum", variable = suspectVar, value = "professor plum", bg=suspectBG, indicatoron = 0).pack(fill="x")
+for suspect in suspects:
+    tk.Radiobutton(suspectFrame, text = suspect.getName(), variable = suspectVar, value = suspect.getName(), bg=suspectBG, indicatoron = 0).pack(fill="x")
 
 #weapon section
 weaponVar = tk.StringVar()
 weaponsBG = "#e18bf7"
 weaponFrame = tk.Frame(swrFrame, bg=swrFrameBG, borderwidth = 1, relief = "groove")
 weaponFrame.pack(side = tk.LEFT, fill="y")
-tk.Radiobutton(weaponFrame, text = "candlestick", variable = weaponVar, value = "candlestick", bg=weaponsBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(weaponFrame, text = "revolver", variable = weaponVar, value = "revolver", bg=weaponsBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(weaponFrame, text = "rope", variable = weaponVar, value = "rope", bg=weaponsBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(weaponFrame, text = "wrench", variable = weaponVar, value = "wrench", bg=weaponsBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(weaponFrame, text = "leadpipe", variable = weaponVar, value = "leadpipe", bg=weaponsBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(weaponFrame, text = "knife", variable = weaponVar, value = "knife", bg=weaponsBG, indicatoron = 0).pack(fill="x")
+for weapon in weapons:
+    tk.Radiobutton(weaponFrame, text = weapon.getName(), variable = weaponVar, value = weapon.getName(), bg=weaponsBG, indicatoron = 0).pack(fill="x")
+
 
 #room section
 roomVar = tk.StringVar()
 roomBG = "#bcf78b"
 roomFrame = tk.Frame(swrFrame, bg=swrFrameBG, borderwidth = 1, relief = "groove")
 roomFrame.pack(side = tk.LEFT, fill="y")
-tk.Radiobutton(roomFrame, text = "study", variable = roomVar, value = "study", bg=roomBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(roomFrame, text = "library", variable = roomVar, value = "library", bg=roomBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(roomFrame, text = "conservatory", variable = roomVar, value = "conservatory", bg=roomBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(roomFrame, text = "hall", variable = roomVar, value = "hall", bg=roomBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(roomFrame, text = "kitchen", variable = roomVar, value = "kitchen", bg=roomBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(roomFrame, text = "ballroom", variable = roomVar, value = "ballroom", bg=roomBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(roomFrame, text = "dining room", variable = roomVar, value = "dining room", bg=roomBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(roomFrame, text = "lounge", variable = roomVar, value = "lounge", bg=roomBG, indicatoron = 0).pack(fill="x")
-tk.Radiobutton(roomFrame, text = "billiard room", variable = roomVar, value = "billiard room", bg=roomBG, indicatoron = 0).pack(fill="x")
+
+for room in rooms:
+    tk.Radiobutton(roomFrame, text = room.getName(), variable = roomVar, value = room.getName(), bg=roomBG, indicatoron = 0).pack(fill="x")
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------
+
 
 
 
