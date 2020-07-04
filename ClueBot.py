3b6f548de784f9c91c08bdeeb954a9008654d740
Peter Gustafson <petergustafson19@gmail.com>
2020-07-03 19:12:17 -0700

    added functionality to deleting the radio buttons when clicking the remove button

--- ClueBot.py
+++ ClueBot.py
@@ -17,34 +17,34 @@ class MainApp(tk.Tk):
         #variables that get passed to the remove button.  These variables hold the current radio button selection.  
         #Need to declare before the button so it has access to these variables and need to declare the remove button
         #before radiobuttons due to formatting issues.
-        suspectVar = tk.StringVar()
-        weaponVar = tk.StringVar()
-        roomVar = tk.StringVar()
+        self.rbVars = {
+            "suspectVar" : tk.StringVar(),
+            "weaponVar" : tk.StringVar(),
+            "roomVar" : tk.StringVar()
+        }
 
         #button to remove the selected options
-        tk.Button(swrFrame, text="remove", command = lambda : self.removeFunction(suspectVar, weaponVar, roomVar)).pack(side=tk.BOTTOM, fill="x")
+        tk.Button(swrFrame, text="remove", command = lambda : self.removeFunction()).pack(side=tk.BOTTOM, fill="x")
 
         #suspect section
-        suspectBG = "#8bf7ec"
         suspectFrame = tk.Frame(swrFrame, bg=swrFrameBG, borderwidth = 1, relief = "groove")
         suspectFrame.pack(side = tk.LEFT, fill="y")
-        for suspect in self.suspects:
-            tk.Radiobutton(suspectFrame, text = suspect.getName(), variable = suspectVar, value = suspect.getName(), bg=suspectBG, indicatoron = 0).pack(fill="x")
 
         #weapon section        
-        weaponsBG = "#e18bf7"
         weaponFrame = tk.Frame(swrFrame, bg=swrFrameBG, borderwidth = 1, relief = "groove")
         weaponFrame.pack(side = tk.LEFT, fill="y")
-        for weapon in self.weapons:
-            tk.Radiobutton(weaponFrame, text = weapon.getName(), variable = weaponVar, value = weapon.getName(), bg=weaponsBG, indicatoron = 0).pack(fill="x")
-
-
+        
         #room section        
-        roomBG = "#bcf78b"
         roomFrame = tk.Frame(swrFrame, bg=swrFrameBG, borderwidth = 1, relief = "groove")
         roomFrame.pack(side = tk.LEFT, fill="y")
-        for room in self.rooms:
-            tk.Radiobutton(roomFrame, text = room.getName(), variable = roomVar, value = room.getName(), bg=roomBG, indicatoron = 0).pack(fill="x")
+
+        self.rbFrames = {
+            "suspectFrame" : suspectFrame,
+            "weaponFrame" : weaponFrame,
+            "roomFrame" : roomFrame
+        }
+        
+        self.updateRadioButtons()
 
         #------------------------------------------------------------------------------------------------------------
 
@@ -75,22 +75,63 @@ class MainApp(tk.Tk):
 
     #function that gets called when the remove button gets click.  This will take the selections from the radio buttons, remove them from our saved
     #list of active cards and update the radiobutton list/chance list
-    def removeFunction(self, suspectVar, weaponVar, roomVar):
+    def removeFunction(self):
         #the following 3 loops will go through the three lists of cards and remove them.
         for i in range(len(self.suspects)):
-            if self.suspects[i].getName() == suspectVar.get():
+            if self.suspects[i].getName() == self.rbVars["suspectVar"].get():
+                self.destroyRadioButton("suspect", self.suspects[i].getName())
                 self.suspects.pop(i)
                 break
         for i in range(len(self.weapons)):
-            if self.weapons[i].getName() == weaponVar.get():
+            if self.weapons[i].getName() == self.rbVars["weaponVar"].get():
+                self.destroyRadioButton("weapon", self.weapons[i].getName())
                 self.weapons.pop(i)
                 break
         for i in range(len(self.rooms)):
-            if self.rooms[i].getName() == roomVar.get():
+            if self.rooms[i].getName() == self.rbVars["roomVar"].get():
+                self.destroyRadioButton("room", self.rooms[i].getName())
                 self.rooms.pop(i)
                 break
 
-        #once we update the lists we need to update the GUI
+        #TODO: update the lists
+        
+    #given the name of the card this fucntion finds the radio button with the same name and removes it from the GUI
+    def destroyRadioButton(self, cardType, cardName):
+        if cardType == "suspect":
+            for child in self.rbFrames["suspectFrame"].children.values():
+                if child.cget("text") == cardName:
+                    child.destroy()
+                    break
+        elif cardType == "weapon":
+            for child in self.rbFrames["weaponFrame"].children.values():
+                if child.cget("text") == cardName:
+                    child.destroy()
+                    break
+        elif cardType == "room":
+            for child in self.rbFrames["roomFrame"].children.values():
+                if child.cget("text") == cardName:
+                    child.destroy()
+                    break
+
+    def updateRadioButtons(self):
+        #selection colors
+        suspectBG = "#8bf7ec"
+        weaponsBG = "#e18bf7"
+        roomBG = "#bcf78b"
+
+        for suspect in self.suspects:
+            tk.Radiobutton(self.rbFrames["suspectFrame"], text = suspect.getName(), variable = self.rbVars["suspectVar"], value = suspect.getName(), bg=suspectBG, indicatoron = 0).pack(fill="x")
+        
+        for weapon in self.weapons:
+            tk.Radiobutton(self.rbFrames["weaponFrame"], text = weapon.getName(), variable = self.rbVars["weaponVar"], value = weapon.getName(), bg=weaponsBG, indicatoron = 0).pack(fill="x")
+
+        for room in self.rooms:
+            tk.Radiobutton(self.rbFrames["roomFrame"], text = room.getName(), variable = self.rbVars["roomVar"], value = room.getName(), bg=roomBG, indicatoron = 0).pack(fill="x")
+
+        
+
+    def updateListBoxes(self):
+        pass
 
 if __name__ == "__main__":
     app = MainApp()
